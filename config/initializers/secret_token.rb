70d4de83282fc8964e5b2d8ac8674da50cd813f5
Liz Hubertz <liz.hubertz@gmail.com>
2016-09-17 11:51:14 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homeworld::Application.config.secret_token = 'f7527d267f443535c942a8bd7029069cb28a06c8fbbb9305882d0fc659b829dcfcc0aa297e1e53c13c400db5db3e2d042eba6db4f4716f8d24de73bf9bbc3574'
